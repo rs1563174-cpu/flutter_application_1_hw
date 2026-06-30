880ce3db8f033032ebe2f241b1bc58fe67776442
rs1563174-cpu <rs1563174@gmail.com>
2026-06-30 12:14:26 +0530

    Text Field+ validation+ Button color

--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -1,26 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _clazzController = TextEditingController();
+
+  String? name;
+  String? clazz;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Search"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Fluttertoast.showToast(
-              msg: "SearchScreen",
-              toastLength: Toast.LENGTH_SHORT,
-            );
-          },
-          child: const Text("Search Button"),
-        ),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hint: Text("Enter your Name"),
+                label: Text("Enter Your Name"),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: _clazzController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hint: Text("Enter your Class"),
+                label: Text("Enter Your Class"),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_nameController.text.isEmpty) {
+                    Fluttertoast.showToast(msg: "Enter Your Name");
+                  } else if (_clazzController.text.isEmpty) {
+                    Fluttertoast.showToast(msg: "Enter Your Class");
+                  } else {
+                    print(_nameController.text);
+                    setState(() {
+                      name = _nameController.text;
+                      clazz = _clazzController.text;
+                    });
+                  }
+                },
+
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+
+                child: Text("Show Name"),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 10),
+          Text("Your Name is: $name and class is: $clazz"),
+        ],
       ),
     );
   }
 }
+
